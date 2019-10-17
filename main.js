82ec836517b49379385a200dd83dd4074874f20a
Reed Kavner <rik919@gmail.com>
2019-10-17 01:36:53 -0400

    working but ugly

---
+++ main.js
@@ -0,0 +1,67 @@
+var sounds = {};
+
+function stopAll(){
+	for (cid in sounds){
+		sounds[cid].stop();
+	}
+}
+
+function init(){
+	const characters = {
+	"bernice" : "Bernice",
+	"boris-and-natasha" : "Boris and Natasha",
+	"britney-and-britney" : "Britney and Britney",
+	"cecile" : "Cecile",
+	"daisy-lee" : "Daisy Lee",
+	"donna" : "Donna",
+	"ezmyr-and-wilde" : "Ezmyr and Wilde",
+	"geraldine" : "Geraldine",
+	"luke-and-stew" : "Luke and Stew",
+	"rick-and-junebug" : "Rick and Junebug"
+	};
+
+
+	$.each(characters, function(cid, name) {
+		// load sound into sound array
+		sounds[cid] = new Howl({
+      		src: ['audio/sample.m4a']
+    	});
+
+		//create the image
+		var $img = $("<img>", {src: "img/thumbs/" + cid +".jpg"});
+
+		//create the label
+		var $label = $("<div>", {class: "label"});
+		$label.text(name);
+
+		//create the box and add the content
+		var $box = $("<div>", {id: cid, class: "col-6 col-md-4 col-lg-3 character"});
+		$box.append($img);
+		$box.append($label);
+
+		//add box to container
+		$('#gallery').append($box);
+	})
+
+	// handle clicks on a character
+	$(".character").click(function(){
+		$char = $(this);
+		var cid = $char.attr('id');
+		if ($char.hasClass('playing')){
+			sounds[cid].stop();
+			$char.removeClass('playing');
+		}else{
+			// stop everything else
+			stopAll();
+			$(".character").removeClass("playing");
+
+			sounds[cid].play();
+			$char.addClass('playing');
+		}
+	})
+}
+
+
+$( document ).ready(function() {
+    init();
+});
